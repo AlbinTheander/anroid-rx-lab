19fef46abdb58d450d483f2c74160839bf6a416b
Albin Theander <Albin.Theander@jayway.com>
2015-11-11 06:49:32 +0100

    Added some rx stuff for searching

--- app/src/main/java/com/jayway/klab/rx/MainActivityFragment.java
+++ app/src/main/java/com/jayway/klab/rx/MainActivityFragment.java
@@ -2,14 +2,35 @@ package com.jayway.klab.rx;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
+import com.jakewharton.rxbinding.widget.TextViewAfterTextChangeEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @Bind(R.id.query)
+    EditText queryView;
+    @Bind(R.id.list)
+    ListView listView;
+    private Subscription subscription;
 
     public MainActivityFragment() {
     }
@@ -17,6 +38,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        subscription = RxTextView.afterTextChangeEvents(queryView)
+                .map(event -> event.editable().toString())
+                .filter(s -> s.length() > 2)
+                .throttleLast(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> Log.d("Test", "Logging " + s));
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        ButterKnife.unbind(this);
+        subscription.unsubscribe();
+        super.onDestroyView();
     }
 }
